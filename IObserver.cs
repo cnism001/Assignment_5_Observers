d3f41e7397f23d5bae00e517c8a8b7e17a05b844
Nikita Smirnov <cnism001@edu.xamk.fi>
2023-11-13 16:21:48 +0200

    Added update method for IObserver class, added CurrentContidionsDisplay subclass

--- IObserver.cs
+++ IObserver.cs
@@ -2,5 +2,19 @@
 {
     public interface IObserver
     {
+        // The Update method that all observers must implement.
+        // This method will be called when WeatherData updates its measurements.
+        void Update(float temperature, float humidity, float pressure, string weatherPrediction);
+    }
+    public class CurrentConditionsDisplay : IObserver
+    {
+        // The method Update is defined here to handle the notification from the WeatherData
+        public void Update(float temperature, float humidity, float pressure, string weatherPrediction)
+        {
+            
+            // Since its a proof-of-concept, it simply prints the current conditions, each on new line.
+            System.Console.WriteLine($"Current Conditions:\n Temperature: {temperature}°C\n " +
+                $"Humidity: {humidity}%\n Pressure: {pressure} hPa\n Weather Prediction: {weatherPrediction}");
+        }
     }
 }
