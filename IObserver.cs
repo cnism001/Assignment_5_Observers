26c9648c769cc9a897d4b19f49fc5f5bd233329e
Nikita Smirnov <cnism001@edu.xamk.fi>
2023-11-13 17:02:37 +0200

    adjusted existing observers to use WeatherDataObject

--- IObserver.cs
+++ IObserver.cs
@@ -4,17 +4,17 @@
     {
         // The Update method that all observers must implement.
         // This method will be called when WeatherData updates its measurements.
-        void Update(float temperature, float humidity, float pressure, string weatherPrediction);
+        void Update(WeatherDataObject data);
     }
     public class CurrentConditionsDisplay : IObserver
     {
         // The method Update is defined here to handle the notification from the WeatherData
-        public void Update(float temperature, float humidity, float pressure, string weatherPrediction)
+        public void Update(WeatherDataObject data)
         {
             
             // Since its a proof-of-concept, it simply prints the current conditions, each on new line.
-            System.Console.WriteLine($"Current Conditions:\n Temperature: {temperature}°C\n " +
-                $"Humidity: {humidity}%\n Pressure: {pressure} hPa\n Weather Prediction: {weatherPrediction}");
+            System.Console.WriteLine($"Current Conditions:\n Temperature: {data.Temperature}°C\n " +
+                $"Humidity: {data.Humidity}%\n Pressure: {data.Pressure} hPa\n Weather Prediction: {data.WeatherPrediction}");
         }
     }
 }
